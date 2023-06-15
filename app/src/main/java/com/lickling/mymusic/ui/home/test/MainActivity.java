48e61f3dca17c5c75fbcc062925cb40a7e2d93f7
Licklin <850698844@qq.com>
2023-06-15 14:38:50 +0800

    改图标，应用名称，添加了一个启动测试页面，添加几个包

---
+++ app/src/main/java/com/lickling/mymusic/ui/home/test/MainActivity.java
@@ -0,0 +1,16 @@
+package com.lickling.mymusic.ui.home.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.lickling.mymusic.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
