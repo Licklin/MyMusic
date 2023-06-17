9e421c40664f2fed27d09fda99b0bbafe99d1863
Licklin <850698844@qq.com>
2023-06-17 22:47:40 +0800

    设置页，完成API页面的ui

--- app/src/main/java/com/lickling/mymusic/ui/setting/api/APIActivity.java
+++ app/src/main/java/com/lickling/mymusic/ui/setting/api/APIActivity.java
@@ -2,24 +2,33 @@ package com.lickling.mymusic.ui.setting.api;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
+import android.widget.Toast;
 
 import com.lickling.mymusic.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class APIActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private RecyclerView recyclerView;
+    private ListAdapter listAdapter;
+    private List<ListItem> listItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_apiactivity);
-
+        setContentView(R.layout.activity_api);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             Window window = getWindow();
             window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
@@ -32,5 +41,29 @@ public class APIActivity extends AppCompatActivity {
                 finish();
             }
         });
+        // 在此处添加菜单项（添加按钮，保存按钮）到Toolbar上
+         getMenuInflater().inflate(R.menu.setting_toolbar_menu, toolbar.getMenu());
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                Toast.makeText(APIActivity.this, "保存", Toast.LENGTH_SHORT).show();
+
+
+                return true;
+            }
+        });
+
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        listItems = new ArrayList<>();
+        // 在此处添加数据到listItems
+        listItems.add(new ListItem("Title 1", "Subtitle 1"));
+        listItems.add(new ListItem("Title 1", "Subtitle 1"));
+        listItems.add(new ListItem("Title 1", "Subtitle 1"));
+
+        listAdapter = new ListAdapter(listItems, this);
+        recyclerView.setAdapter(listAdapter);
     }
 }
