d3101c8d311ecfff1cbb27031c649408556a0051
polpsafa <2502120998@qq.com>
2023-06-21 19:37:43 +0800

    主界面改善

---
+++ app/src/main/java/com/lickling/mymusic/ui/home/PQ/Desktop_Listltem.java
@@ -0,0 +1,20 @@
+package com.lickling.mymusic.ui.home.PQ;
+
+public class Desktop_Listltem {
+    private String title;
+    private String subtitle;
+
+    public Desktop_Listltem(String title, String subtitle) {
+        this.title = title;
+        this.subtitle = subtitle;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+}
+
